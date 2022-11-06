38b159f57926064bc5cb6b54dbca256195e16a7c
ccruzpach <ccruzpach@gmail.com>
2022-11-06 03:25:49 -0500

    API work and Interface

--- config/services.php
+++ config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'mailchip' => [
+        'key' => env('MAILCHIP_KEY'),
+        'lists' => [
+            'subscribers' => env('MAILCHIP_LIST_SUBSCRIBERS')
+        ]
+    ]
 ];
--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,16 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NewsletterController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
-
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
