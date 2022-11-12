2f25c431a62de99ad2687bda39b694b223556285
ccruzpach <ccruzpach@gmail.com>
2022-11-12 16:09:35 -0500

    Refactoring and password hide fix

--- resources/views/components/form/password-toggle.blade.php
+++ resources/views/components/form/password-toggle.blade.php
@@ -1,1 +1,12 @@
 <input type="checkbox" onclick="togglePassword()" class="mt-4"> Show Password
+
+<script>
+    function togglePassword() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+</script>
